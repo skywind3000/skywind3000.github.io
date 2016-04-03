2a87410df3f08303adcf72704861fe34a9bfefb5
skywind3000 <skywind3000@gmail.com>
2016-04-03 23:37:18 +0800

    update

--- vim/install.sh
+++ vim/install.sh
@@ -4,10 +4,8 @@ DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 cd $DIR
 
 mkdir -p ~/.vim
-ln -s $DIR/vimneat.vim ~/.vim/vimneat.vim
-ln -s $DIR/vimmake.vim ~/.vim/vimmake.vim
-ln -s $DIR/skywind.vim ~/.vim/skywind.vim
-ln -s $DIR/backup.vim ~/.vim/backup.vim
+ln -s $DIR/asc ~/.vim/asc
+ln -s $DIR/asc.vim ~/.vim/asc.vim
 ln -s $DIR/plugin ~/.vim/plugin
 ln -s $DIR/autoload ~/.vim/autoload
 ln -s $DIR/doc ~/.vim/doc
