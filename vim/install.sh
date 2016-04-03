2baa7a141172d411759868e8c6a8cbe959b9cd51
skywind3000 <skywind3000@gmail.com>
2016-04-03 23:37:57 +0800

    update

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,8 @@ cd $DIR
 mkdir -p ~/.vim
 ln -s $DIR/asc ~/.vim/asc
 ln -s $DIR/asc.vim ~/.vim/asc.vim
+ln -s $DIR/skywind.vim ~/.vim/skywind.vim
+
 ln -s $DIR/plugin ~/.vim/plugin
 ln -s $DIR/autoload ~/.vim/autoload
 ln -s $DIR/doc ~/.vim/doc
