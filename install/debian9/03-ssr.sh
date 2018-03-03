6722c69f1d9ecd93d12e3883363765a76eff49b9
skywind3000 <skywind3000@163.com>
2018-03-03 22:42:50 +0800

    commit new ssr

--- install/debian9/03-ssr.sh
+++ install/debian9/03-ssr.sh
@@ -5,20 +5,20 @@ git clone https://github.com/skywind3000/ssr.git
 
 cd /etc/supervisor/conf.d
 
-echo "[program:ssr]" > ssr.conf
-echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-256-cfb -O auth_aes128_sha1 -o tls1.2_ticket_auth_compatible --fast-open --workers 2" >> ssr.conf
-echo "autorestart=true" >> ssr.conf
-echo "user=nobody" >> ssr.conf
+echo "[program:ssr]" > ssr.disable
+echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-256-cfb -O origin -o plain --fast-open --workers 2" >> ssr.disable
+echo "autorestart=true" >> ssr.disable
+echo "user=nobody" >> ssr.disable
 
-echo "[program:ssr-km]" > ssr-km.conf
-echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-128-cfb  -o tls1.2_ticket_auth --fast-open --workers 2" >> ssr-km.conf
-echo "autorestart=true" >> ssr-km.conf
-echo "user=nobody" >> ssr-km.conf
+echo "[program:ssr-km]" > ssr-km.disable
+echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-128-cfb  -o tls1.2_ticket_auth --fast-open --workers 2" >> ssr-km.disable
+echo "autorestart=true" >> ssr-km.disable
+echo "user=nobody" >> ssr-km.disable
 
-echo "[program:ssr-test]" > ssr-test.conf
-echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible --fast-open --workers 2" >> ssr-test.conf
-echo "autorestart=true" >> ssr-test.conf
-echo "user=nobody" >> ssr-test.conf
+echo "[program:ssr-test]" > ssr-test.disable
+echo "command=/usr/bin/python2 /home/data/software/ssr/shadowsocks/server.py -p XXXX -k XXXXXXX -m aes-256-cfb -O origin -o plain --fast-open --workers 2" >> ssr.disable
+echo "autorestart=true" >> ssr-test.disable
+echo "user=nobody" >> ssr-test.disable
 
 # supervisorctl reload
 
